ac92cac2d256b5cb52f9170277fcb791626cb9dc
sqz269 <yin2690855381@gmail.com>
2023-01-04 21:02:17 -0500

    added thwikicc cache

---
+++ InfoProviders/ThcInfoProvider/ThcSongInfoProvider/Cached/thc___Good_R.A.C
@@ -0,0 +1,191 @@
+{{同人专辑头部}}
+
+== 专辑信息 ==
+{{同人专辑信息|
+| 封面 = {{PAGENAME}}封面.jpg
+| 封面角色 = 无
+| 名称 = Good R.A.C.
+| 译名 = 
+| 制作方 = Reset All Controllers
+| 展会 = {{CM|3|89}}
+| 编号 = RAC-0010
+| 类型 = 精选集，
+| 风格类型 = Rock，
+| 会场售价 = 1000日元
+| 通贩售价 = 1296日元
+| 通贩售价补充 = （下载）
+| 官网页面 = http://www.geocities.jp/adazakura_midi/discography/goodrac.html，http://reset-all-controllers.com/2015/12/27/good-r-a-c/
+| 
+}}
+{{通贩列表|
+| 官方 = 
+{{通贩网址|
+| 类型 = DLsite
+| 编号 = RJ169566
+| 标题 = 
+}}
+
+}}
+
+== Staff ==
+{{专辑人员列表|
+|碟号=1
+|替换=
+徒桜 = Reset All Controllers
+大兎オバマ = Reset All Controllers
+
+}}
+; Illustration : もこきよ （[http://www.pixiv.net/member.php?id=2757229 Pixiv]）
+
+== 试听 ==
+{{专辑音频|
+| 类型 = SoundCloud
+| 编号 = 239270161
+| 标题 = 
+}}
+
+== 曲目列表 ==
+{{专辑曲目列表|
+| 嵌套 =
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = Bright.
+|原名称 = Introduction ～ The World Without Light of Faith
+|名称 = Introduction ～ The World Without Lights of Faith!!(Remake)
+|时长 = 
+|编曲 = 徒桜
+|原曲 = {{风神录音乐名|3|1}}，
+|重制 = 是
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = THE BEAST
+|原名称 = WILD RUSH
+|名称 = WILD RUSH(Remake)
+|时长 = 
+|编曲 = 徒桜
+|原曲 = {{妖妖梦音乐名|3|5}}，
+|重制 = 是
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = THE BEAST
+|原名称 = Sorrowful Guitar
+|名称 = Sorrowful Guitar(Remake)
+|时长 = 
+|编曲 = 大兎オバマ
+|原曲 = {{星莲船音乐名|3|2}}，
+|重制 = 是
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = Bright.
+|原名称 = Prey ～ Search for Light
+|名称 = Prey ～ Search for Light(Remix)
+|时长 = 
+|编曲 = 徒桜
+|原曲 = {{风神录音乐名|3|4}}，
+|重制 = 是
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = Bright.
+|原名称 = Miracle ☆ Twinkle
+|名称 = Miracle ☆ Twinkle
+|时长 = 
+|编曲 = 大兎オバマ
+|原曲 = {{风神录音乐名|3|10}}，
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = THE BEAST Ⅱ
+|原名称 = AMATERASU ～ Ground Nova
+|名称 = AMATERASU ～ Ground Nova(Remake)
+|时长 = 
+|编曲 = 徒桜
+|原曲 = {{地灵殿音乐名|3|13}}，
+|重制 = 是
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = THE BEAST Ⅱ
+|原名称 = 250ml of Weak Acid
+|名称 = 250ml of Weak Acid
+|时长 = 
+|编曲 = 大兎オバマ
+|原曲 = {{永夜抄音乐名|3|17}}，
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = THE BEAST Ⅱ
+|原名称 = Prime Number of Esoteria
+|名称 = Prime Number of Esoteria
+|时长 = 
+|编曲 = 大兎オバマ
+|原曲 = {{星莲船音乐名|3|10}}，
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = Meteorlights
+|原名称 = Complete Darkness　～ The Missing Revenger.
+|名称 = Complete Darkness　～ The Missing Revenger.(Remix)
+|时长 = 
+|编曲 = 徒桜
+|原曲 = {{封魔录音乐名|3|11}}，
+|重制 = 是
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = Resurrection
+|原名称 = The Flower Imperial Court
+|名称 = The Flower Imperial Court
+|时长 = 
+|编曲 = 大兎オバマ
+|原曲 = {{妖妖梦音乐名|3|8}}，
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = Memory
+|原名称 = Nightmare Bell
+|名称 = Nightmare Bell
+|时长 = 
+|编曲 = 大兎オバマ
+|原曲 = {{文花帖DS音乐名|3|5}}，
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = Re:Memory
+|原名称 = Memories of Phantasmagoria
+|名称 = Memories of Phantasmagoria(Remix)
+|时长 = 
+|编曲 = 徒桜
+|原曲 = {{妖妖梦音乐名|3|18}}，
+|重制 = 是
+}}
+{{同人曲目信息|
+|名称 = Red Aggressive Clash!!
+|时长 = 
+|编曲 = 大兎オバマ
+|原曲 = {{永夜抄音乐名|3|9}}，
+}}
+{{同人曲目信息|
+|名称 = The Black Mage
+|时长 = 
+|编曲 = 徒桜
+|原曲 = {{永夜抄音乐名|3|10}}，
+}}
+{{同人曲目信息|
+|社团 = Reset All Controllers
+|原专辑 = A-L-I-C-E
+|原名称 = Wonderland ～ Child's View
+|名称 = Wonderland ～ Child’s View
+|时长 = 
+|编曲 = 徒桜
+|原曲 = {{怪绮谈音乐名|3|16}}，
+}}
+
+}}
+
+== 评论 ==
+
+{{Bottom}}
---
+++ InfoProviders/ThcInfoProvider/ThcSongInfoProvider/Cached/thc___Plastic_Love_e.p
@@ -0,0 +1,84 @@
+{{同人专辑头部}}
+
+== 专辑信息 ==
+{{同人专辑信息|
+| 封面 = jpg
+| 封面角色 = {{爱丽丝}}
+| 名称 =Plastic Love e.p. 
+| 译名 = 
+| 制作方 = Further Ahead Of Warp
+| 展会 = {{M3|3|32}}
+| 编号 = 
+| 风格类型 = 硬核
+| 类型 = EP
+| 会场售价 = 300日元
+| 通贩售价 = 400日元
+| 官网页面 = http://furtheraheadofwarp.com/old/plasticlove.html
+}}
+
+== Staff ==
+{{专辑人员列表|
+|碟号=1
+|替换=
+縫糸=Further Ahead Of Warp
+きょーこ=mumeria*
+}}
+; Illustration :  九条ナツメ（[[pu:459874]])
+
+== 试听 ==
+{{专辑音频|
+| 类型 = url
+| 编号 = http://furtheraheadofwarp.com/old/mp3/plasticloveepxfd.mp3
+| 标题 = XFD试听
+}}
+{{专辑音频|
+| 类型 = SoundCloud
+| 编号 = 116999952
+}}
+
+== 曲目列表 ==
+{{专辑曲目列表|
+| 嵌套 =
+{{同人曲目信息|
+|名称 = I'm Alone
+|时长 = 04:35
+|编曲 = 縫糸
+|演唱 = きょーこ，縫糸
+|作词 = 縫糸
+|原曲 = {{妖妖梦音乐名|3|7}}
+
+}}
+{{同人曲目信息|
+|名称 = Falling Bouquets
+|时长 = 05:34
+|编曲 = 縫糸
+|演唱 = きょーこ，縫糸
+|作词 = 縫糸
+|原曲 = {{风神录音乐名|3|4}}
+
+}}
+{{同人曲目信息|
+|名称 = A Selfish Princess Draws Her Candy House On The Drawing Paper -The Faded Diary-
+|时长 = 04:52
+|编曲 = 縫糸
+|演唱 = きょーこ，縫糸
+|作词 = 縫糸
+|原曲 = {{文花帖音乐名|3|6}}
+
+}}
+{{同人曲目信息|
+|名称 = Mother
+|时长 = 07:39
+|编曲 = 縫糸
+|演唱 = きょーこ，縫糸
+|作词 = 縫糸
+
+}}
+}}
+
+== 评论 ==
+
+
+
+
+{{Bottom}}
